0f0fbbf66e5a0ecb677f693fed80ea67d9091cfe
tilak-dev <tsingh99991@gmail.com>
2025-03-10 13:51:48 +0530

    bottom navigation and enhance layout with category selection

--- frontend/src/components/app-ui/Header.tsx
+++ frontend/src/components/app-ui/Header.tsx
@@ -5,50 +5,75 @@ import { IoClose, IoSearchOutline } from "react-icons/io5";
 import { BiUser } from "react-icons/bi";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { FiHeart } from "react-icons/fi";
+import { FaChevronDown } from "react-icons/fa";
+import { bottomNavigation } from "@/data/header";
 
 export default function Header() {
   const [searchText, setSearchText] = useState<string | null>(null);
   return (
-    <header className="w-full bg-whiteText ">
-      <Container className="h-20 flex items-center justify-between md:px-4">
-        {/* logo */}
-        <img src={logo} alt="logo" className="w-44" />
-        {/* search bar */}
-        <div className="hidden md:inline-flex max-w-3xl w-full relative">
-          <input
-            type="text"
-            className="appearance-none w-full flex-1 rounded-full text-gray-900 text-lg placeholder:text-base placeholder:tracking-wide shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-normal focus:ring-darkText sm:text-sm px-4 py-2"
-            placeholder="Search products..."
-            onChange={(e) => setSearchText(e.target.value)}
-            value={searchText ? searchText : ""}
-          />
-          {searchText ? (
-            <IoClose
-              onClick={() => setSearchText(null)}
-              className=" absolute top-2.5 right-4 text-xl hover:text-red-500 cursor-pointer duration-200"
-            />
-          ) : (
-            <IoSearchOutline className="absolute top-2.5 right-4 text-xl" />
-          )}
-        </div>
-        {/* menu  */}
-        <div className="flex gap-x-6 text-center text-2xl ">
-          <BiUser className="hover:text-skyText duration-200 cursor-pointer" />
-          <div className="relative block">
-            <FiHeart className="hover:text-skyText duration-200 cursor-pointer" />
-            <span className="absolute inline-flex items-center justify-center -top-1 -right-2 text-[9px] rounded-full size-4 bg-redText text-whiteText">
-              0
-            </span>
-          </div>
+    <>
+      <header>
+        {/* first header */}
+        <div className="w-full bg-whiteText ">
+          <Container className="h-20 flex items-center justify-between md:px-4">
+            {/* logo */}
+            <img src={logo} alt="logo" className="w-44" />
+            {/* search bar */}
+            <div className="hidden md:inline-flex max-w-3xl w-full relative">
+              <input
+                type="text"
+                className="appearance-none w-full flex-1 rounded-full text-gray-900 text-lg placeholder:text-base placeholder:tracking-wide shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-normal focus:ring-darkText sm:text-sm px-4 py-2"
+                placeholder="Search products..."
+                onChange={(e) => setSearchText(e.target.value)}
+                value={searchText ? searchText : ""}
+              />
+              {searchText ? (
+                <IoClose
+                  onClick={() => setSearchText(null)}
+                  className=" absolute top-2.5 right-4 text-xl hover:text-red-500 cursor-pointer duration-200"
+                />
+              ) : (
+                <IoSearchOutline className="absolute top-2.5 right-4 text-xl" />
+              )}
+            </div>
+            {/* menu  */}
+            <div className="flex gap-x-6 text-center text-2xl ">
+              <BiUser className="hover:text-skyText duration-200 cursor-pointer" />
+              <div className="relative block">
+                <FiHeart className="hover:text-skyText duration-200 cursor-pointer" />
+                <span className="absolute inline-flex items-center justify-center -top-1 -right-2 text-[9px] rounded-full size-4 bg-redText text-whiteText">
+                  0
+                </span>
+              </div>
 
-          <div className="relative block">
-            <RiShoppingCart2Line className="hover:text-skyText duration-200 cursor-pointer" />
-            <span className="absolute inline-flex items-center justify-center -top-1 -right-2 text-[9px] rounded-full size-4 bg-redText text-whiteText">
-              0
-            </span>
-          </div>
+              <div className="relative block">
+                <RiShoppingCart2Line className="hover:text-skyText duration-200 cursor-pointer" />
+                <span className="absolute inline-flex items-center justify-center -top-1 -right-2 text-[9px] rounded-full size-4 bg-redText text-whiteText">
+                  0
+                </span>
+              </div>
+            </div>
+          </Container>
         </div>
-      </Container>
-    </header>
+
+        {/* second header */}
+        <div className="w-full bg-darkText text-whiteText ">
+          {" "}
+          <Container className="py-2  md:px-4 flex items-center gap-5 justify-between max-w-4xl ">
+            <p className="flex items-center gap-1">
+              Select Category <FaChevronDown />
+            </p>
+            {bottomNavigation.map((nav) => (
+              <span
+              className="uppercase hidden md:inline-flex text-sm font-semibold text-whiteText/90 hover:text-whiteText duration-200 relative hover:underline hover:transition-shadow cursor-pointer"
+                key={nav.link}
+              >
+                {nav.title}
+              </span>
+            ))}
+          </Container>
+        </div>
+      </header>
+    </>
   );
 }
