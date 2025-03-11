d635b33d25f1762dcd3ef31f84bd9af8f68a681d
tilak-dev <tsingh99991@gmail.com>
2025-03-11 15:22:37 +0530

    Add Footer and FooterTop components with incentives data; enhance layout and styling

--- frontend/src/components/app-ui/Footer.tsx
+++ frontend/src/components/app-ui/Footer.tsx
@@ -1,7 +1,15 @@
+import { payment } from "@/assets";
+import Container from "./Container";
+import FooterTop from "./FooterTop";
+
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <div className="mt-10">
+      <FooterTop />
+      <Container className="flex flex-col md:flex-row items-center gap-4 justify-between md:px-4">
+        <p>@2024 E-Commerce Solutions. All rights reserved.</p>
+        <img src={payment} alt="payment-img"  className="object-cover"/>
+      </Container>
     </div>
   )
 }
---
+++ frontend/src/data/footer.ts
@@ -0,0 +1,22 @@
+import { exchangeIcon, shippingIcon, warrantyCard } from "@/assets";
+
+export const incentives = [
+    {
+      name: "Free shipping",
+      imageSrc: shippingIcon,
+      description:
+        "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+    },
+    {
+      name: "10-year warranty",
+      imageSrc: warrantyCard,
+      description:
+        "If it breaks in the first 10 years we'll replace it. After that you're on your own though.",
+    },
+    {
+      name: "Exchanges",
+      imageSrc: exchangeIcon,
+      description:
+        "If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.",
+    },
+  ];
