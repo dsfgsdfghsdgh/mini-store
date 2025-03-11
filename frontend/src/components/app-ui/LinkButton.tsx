4f81068a6ea8e276babde3422de41bd5d438483b
tilak-dev <tsingh99991@gmail.com>
2025-03-11 16:41:53 +0530

    Add NotFound page with enhanced UI and LinkButton component for navigation

---
+++ frontend/src/components/app-ui/LinkButton.tsx
@@ -0,0 +1,23 @@
+import { mergedefaultCss } from "@/lib/mergeCustomTailwindCss";
+import { Link } from "react-router-dom";
+
+type LinkProps = {
+  to?: string;
+  children?: React.ReactNode;
+  className?: string;
+};
+
+export default function LinkButton({ children, className, to }: LinkProps) {
+  return (
+    <Link
+      replace={true}
+      to={to ? to : "/"}
+      className={mergedefaultCss(
+        " bg-darkText/80 hover:bg-darkText text-whiteText py-2.5 px-6 rounded-full flex items-center text-center gap-2 duration-200",
+        className
+      )}
+    >
+      {children ? children : "Shopping"}
+    </Link>
+  );
+}
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,20 @@
+import LinkButton from "@/components/app-ui/LinkButton";
+import { FaExclamationTriangle } from "react-icons/fa";
+import { useLocation} from "react-router-dom";
+
 export default function NotFound() {
+  const location = useLocation();
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>Sorry, the page you're looking for doesn't exist.</p>
-      <a href="/">Go back to home</a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center space-y-3 px-6 py-10">
+      <FaExclamationTriangle className="text-red-500 text-5xl sm:text-6xl md:text-7xl mb-4" />
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-800">
+        {location.pathname} - Page Not Found
+      </h1>
+      <p className="text-gray-600 mt-2 max-w-md sm:max-w-lg md:max-w-xl">
+        Oops! The page you're looking for doesn't exist or has been moved.
+      </p>
+
+      <LinkButton className="text-sm mt-5">Go back to Home</LinkButton>
     </div>
-  )
+  );
 }
